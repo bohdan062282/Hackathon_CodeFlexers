81ed6416f179404d1a6647fb712a6ffa9058a491
Vladyslav Kotlyarenko <v.kotlyarenko@mobidev.biz>
2021-05-06 13:07:43 +0300

    small refactoring

---
+++ src/i18n.js
@@ -0,0 +1,27 @@
+export const TRANSLATION = [
+  {
+    lang: "ru",
+    questions: {
+      question1: "-",
+      question2: "-",
+    },
+    criterions: {
+      TENSION_CIRCUMSTANCES: "Переживание психотравмирующих обстоятельств",
+    },
+  },
+  {
+    lang: "en",
+    questions: {
+      question1:
+        "Organizational flaws at work constantly make you nervous, worried, tense.",
+      question2:
+        "Today I am as satisfied with my profession as I was at the beginning of my career.",
+      question3:
+        "I made a mistake in choosing a profession or profile (I am not in my place).",
+      question4:
+        "My concern is that I have begun to work worse (less productive, better quality, slower).",
+      question5:
+        "The warmth of interaction with partners very much depends on my mood - good or bad.",
+    },
+  },
+];
